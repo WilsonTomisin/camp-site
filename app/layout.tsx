643e8641523aba6d313b6aaf9e057053971d7820
Wilson Tomisin <tommywilson972@gmail.com>
2023-12-09 21:05:52 +0100

    finished footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,11 +18,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Navbar/>
-      <main className=' relative overflow-hidden '>
-        {children}
-      </main>
-      <Footer/>
+      <body>
+        <Navbar/>
+        <main className=' relative overflow-hidden '>
+          {children}
+        </main>
+        <Footer/>
+      </body>
     </html>
   )
 }
