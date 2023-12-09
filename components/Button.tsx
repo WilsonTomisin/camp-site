0bdc2500ff91c4e46a2e3b5ecc9ae6c64fa08fb7
Wilson Tomisin <tommywilson972@gmail.com>
2023-12-09 18:55:44 +0100

    finished getapp section

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,11 +5,12 @@ type ButtonProps = {
     title : string ;
     icon?: string ;
     variant: string ;
+    full?: boolean
 }
 
-export const Button = ({title,variant,icon,type}: ButtonProps) => {
+export const Button = ({title,variant,icon,type,full}: ButtonProps) => {
   return (
-    <button type={type} className={` ${variant} flexCenter rounded-full gap-3`}>
+    <button type={type} className={` ${variant} flexCenter rounded-full gap-3 ${ full && ' w-full' }`}>
         <span>{icon && <Image src={icon} alt={title} width={24} height={24}/>}</span>
         <span className=" bold-16">{title}</span>
     </button>
